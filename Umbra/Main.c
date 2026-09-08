87a1fb304f6d821826ad6a9202664a3be59b59ea
Carlos Enriquez <carlos.enriquez.valerio@gmail.com>
2026-09-07 18:28:31 -0600

    Set Window Full Screen

--- Umbra/Main.c
+++ Umbra/Main.c
@@ -48,6 +48,8 @@ bool initialize_window(void)
 		fprintf(stderr, "Error creating SDL Renderer\n");
 		return false;
 	}
+
+	SDL_SetWindowFullscreen(window, SDL_WINDOW_FULLSCREEN);
 	return true;
 }
 void setup(void)
